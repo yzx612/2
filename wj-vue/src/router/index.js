ff574ca2397a57c6c333db454ccae9ef94be69e9
yzx <704516389@qq.com>
2020-06-22 00:01:40 +0800

    1

--- wj-vue/src/router/index.js
+++ wj-vue/src/router/index.js
@@ -1,9 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-// 导入刚才编写的组件
-import AppIndex from '@/components/home/AppIndex'
-import Login from '@/components/Login'
 
 Vue.use(Router)
 
@@ -13,17 +10,6 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
-    },
-    // 下面都是固定的写法
-    {
-      path: '/login',
-      name: 'Login',
-      component: Login
-    },
-    {
-      path: '/index',
-      name: 'AppIndex',
-      component: AppIndex
     }
   ]
 })
